dbf27b0659a5ea261d772424fbce2cddae6b2060
Johan <56724011+risujas@users.noreply.github.com>
2024-05-01 21:53:15 +0300

    Improve AS selecting

--- Assets/Scripts/AudioSourceSelector.cs
+++ Assets/Scripts/AudioSourceSelector.cs
@@ -6,16 +6,34 @@ public class AudioSourceSelector : MonoBehaviour
 {
 	[SerializeField] private LayerMask audioSourceLayerMask;
 
+	private List<AudioSourceController> selectedControllers = new List<AudioSourceController>();
+
 	private void Update()
 	{
 		if (Input.GetMouseButtonDown(0))
 		{
+			bool multiSelect = Input.GetKey(KeyCode.LeftControl);
+
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, out RaycastHit hit, 100.0f, audioSourceLayerMask))
 			{
 				var audioSourceController = hit.collider.gameObject.GetComponentInParent<AudioSourceController>();
 				if (audioSourceController)
 				{
+					if (!multiSelect)
+					{
+						for (int i = selectedControllers.Count - 1; i >= 0; i--)
+						{
+							selectedControllers[i].EnableInfoPanel(false);
+							selectedControllers.Remove(selectedControllers[i]);
+						}
+					}
+
+					if (!selectedControllers.Contains(audioSourceController))
+					{
+						selectedControllers.Add(audioSourceController);
+					}
+
 					audioSourceController.EnableInfoPanel(true);
 				}
 			}
