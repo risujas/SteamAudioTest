f3df397b8a32cc70054cbe3cd2f1afad3e251fa0
Johan <56724011+risujas@users.noreply.github.com>
2024-05-01 20:46:26 +0300

    Add distance display

--- Assets/Scripts/AudioSourceController.cs
+++ Assets/Scripts/AudioSourceController.cs
@@ -11,8 +11,11 @@ public class AudioSourceController : MonoBehaviour
 	[SerializeField] private Canvas canvas;
 	[SerializeField] private RectTransform panel;
 	[SerializeField] private TextMeshProUGUI clipNameText;
+	[SerializeField] private TextMeshProUGUI distanceText;
 
 	private AudioSource audioSource;
+	private AudioListener audioListener;
+
 	private float finishTime = 0.0f;
 	private bool started = false;
 
@@ -24,6 +27,7 @@ public class AudioSourceController : MonoBehaviour
 	private void Start()
 	{
 		audioSource = GetComponent<AudioSource>();
+		audioListener = GameObject.FindGameObjectWithTag("AudioListener").GetComponent<AudioListener>();
 
 		if (audioSource.playOnAwake)
 		{
@@ -49,6 +53,7 @@ public class AudioSourceController : MonoBehaviour
 		{
 			panel.transform.position = Camera.main.WorldToScreenPoint(audioSource.transform.position);
 			clipNameText.text = "Clip: " + audioSource.clip.name;
+			distanceText.text = string.Format("Distance: {0:0.00}m", Vector3.Distance(audioSource.transform.position, audioListener.transform.position));
 		}
 	}
 }
