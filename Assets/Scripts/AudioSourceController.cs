e2f13548cd3dbcbe09d0e3e26610dd9febf05ac7
Johan <56724011+risujas@users.noreply.github.com>
2024-05-02 21:58:59 +0300

    Update AudioSourceController.cs

--- Assets/Scripts/AudioSourceController.cs
+++ Assets/Scripts/AudioSourceController.cs
@@ -59,9 +59,14 @@ public class AudioSourceController : MonoBehaviour
 	{
 		if (panel.gameObject.activeInHierarchy)
 		{
-			if (Input.GetKey(KeyCode.M))
+			if (Input.GetKey(KeyCode.M) || Input.GetMouseButton(1))
 			{
 				transform.position += new Vector3(Input.mousePositionDelta.x, 0.0f, Input.mousePositionDelta.y) * dragSpeed * Time.deltaTime;
+			}
+
+			if (Input.GetKey(KeyCode.H) || Input.GetMouseButton(2))
+			{
+				transform.position += new Vector3(0.0f, Input.mousePositionDelta.y, 0.0f) * dragSpeed * Time.deltaTime;
 			}
 
 			if (Input.GetKeyDown(KeyCode.LeftArrow))
