825083c1092e0fee127f6415da060586e4fc1e34
Johan <56724011+risujas@users.noreply.github.com>
2024-05-09 06:31:03 +0300

    Fully implement deletion and creation

--- Assets/Scripts/AudioSourceManager.cs
+++ Assets/Scripts/AudioSourceManager.cs
@@ -15,6 +15,7 @@ public class AudioSourceManager : MonoBehaviour
 	[SerializeField] private GameObject placementIndicatorPrefab;
 	[SerializeField] private LayerMask placementSurfaceLayer;
 	[SerializeField] private TextMeshProUGUI placementButtonText;
+	[SerializeField] private TextMeshProUGUI deletionButtonText;
 
 	[Header("Selection")]
 	[SerializeField] private LayerMask audioSourceLayerMask;
@@ -22,17 +23,27 @@ public class AudioSourceManager : MonoBehaviour
 
 	private List<AudioSourceController> selectedControllers = new List<AudioSourceController>();
 	private GameObject placementIndicator;
-
 	private bool isPlacingController;
+	private bool isDeletingController;
 
 	public void TogglePlacement()
 	{
-		if (!isPlacingController)
+		EnablePlacement(!isPlacingController);
+	}
+
+	public void ToggleDeletion()
+	{
+		EnableDeletion(!isDeletingController);
+	}
+
+	public void EnablePlacement(bool enable)
+	{
+		isPlacingController = enable;
+
+		if (isPlacingController)
 		{
 			placementButtonText.text = "Cancel";
 			placementButtonText.fontStyle = FontStyles.Italic;
-
-			isPlacingController = true;
 
 			if (placementIndicator == null)
 			{
@@ -44,12 +55,36 @@ public class AudioSourceManager : MonoBehaviour
 			placementButtonText.text = "Create";
 			placementButtonText.fontStyle = FontStyles.Normal;
 
-			isPlacingController = false;
-
 			if (placementIndicator)
 			{
 				Destroy(placementIndicator);
 			}
+		}
+
+		if (enable)
+		{
+			EnableDeletion(false);
+		}
+	}
+
+	public void EnableDeletion(bool enable)
+	{
+		isDeletingController = enable;
+
+		if (isDeletingController)
+		{
+			deletionButtonText.text = "Cancel";
+			deletionButtonText.fontStyle = FontStyles.Italic;
+		}
+		else
+		{
+			deletionButtonText.text = "Delete";
+			deletionButtonText.fontStyle = FontStyles.Normal;
+		}
+
+		if (enable)
+		{
+			EnablePlacement(false);
 		}
 	}
 
@@ -123,52 +158,60 @@ public class AudioSourceManager : MonoBehaviour
 
 	private void HandlePlacement()
 	{
-		if (isPlacingController)
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out RaycastHit hit, 100.0f, placementSurfaceLayer))
 		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray, out RaycastHit hit, 100.0f, placementSurfaceLayer))
-			{
-				placementIndicator.transform.position = hit.point;
+			placementIndicator.transform.position = hit.point;
 
-				if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
-				{
-					Instantiate(audioControllerPrefab, hit.point + Vector3.up, Quaternion.identity);
-					TogglePlacement();
-				}
+			if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+			{
+				Instantiate(audioControllerPrefab, hit.point + Vector3.up, Quaternion.identity);
+				EnablePlacement(false);
+			}
+		}
+	}
+
+	private void HandleDeletion()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out RaycastHit hit, 100.0f, audioSourceLayerMask))
+		{
+			if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+			{
+				var controller = hit.collider.gameObject.GetComponentInParent<AudioSourceController>();
+				Destroy(controller.gameObject);
+				EnableDeletion(false);
 			}
 		}
 	}
 
 	private void HandleSelection()
 	{
-		if (objectSelector.CompletedSelection)
+		bool multiSelect = Input.GetKey(KeyCode.LeftControl);
+
+		var selection = objectSelector.SelectedObjects;
+		if (selection.Count > 0)
 		{
-			bool multiSelect = Input.GetKey(KeyCode.LeftControl);
-
-			var selection = objectSelector.SelectedObjects;
-			if (selection.Count > 0)
-			{
-				if (!multiSelect)
-				{
-					DeselectControllers();
-				}
-
-				foreach (var o in selection)
-				{
-					var audioSourceController = o.GetComponent<AudioSourceController>();
-					if (audioSourceController == null) audioSourceController = o.GetComponentInParent<AudioSourceController>();
-
-					if (audioSourceController)
-					{
-						audioSourceController.EnableInfoPanel(true);
-						selectedControllers.Add(audioSourceController);
-					}
-				}
-			}
-			else
+			if (!multiSelect)
 			{
 				DeselectControllers();
 			}
+
+			foreach (var o in selection)
+			{
+				var audioSourceController = o.GetComponent<AudioSourceController>();
+				if (audioSourceController == null) audioSourceController = o.GetComponentInParent<AudioSourceController>();
+
+				if (audioSourceController)
+				{
+					audioSourceController.EnableInfoPanel(true);
+					selectedControllers.Add(audioSourceController);
+				}
+			}
+		}
+		else
+		{
+			DeselectControllers();
 		}
 	}
 
@@ -178,7 +221,11 @@ public class AudioSourceManager : MonoBehaviour
 		{
 			HandlePlacement();
 		}
-		else
+		else if (isDeletingController)
+		{
+			HandleDeletion();
+		}
+		else if (objectSelector.CompletedSelection)
 		{
 			HandleSelection();
 		}
